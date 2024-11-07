7549e4e0ce1bcbb7e090faa57cff40f021bab1d7
eslamdyab <eslamdyab22@gmail.com>
2024-11-07 16:29:50 +0200

    bash command for downloading the pdf file from the website

---
+++ Tadawul-Exchange-Website/wget.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+wget -P pdfs --user-agent="Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:132.0) Gecko/20100101 Firefox/132.0" "https://www.saudiexchange.sa/wps/wcm/connect/c4f67241-1068-48ba-b5c6-6276a4ca77ae/Monthly+Trading+and+Ownership+By+Nationality+Report+31-10-2023.pdf?MOD=AJPERES&CACHEID=ROOTWORKSPACE-c4f67241-1068-48ba-b5c6-6276a4ca77ae-oP52nbM"
