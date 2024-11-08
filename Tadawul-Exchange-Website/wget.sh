063f6eff94bd9665a73838eb085324fb3d66440d
eslamdyab <eslamdyab22@gmail.com>
2024-11-08 15:21:31 +0200

    update bash scritp to take arguments like pdf url and pdf file name when downloaded

--- Tadawul-Exchange-Website/wget.sh
+++ Tadawul-Exchange-Website/wget.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 pdf_url="$1"
-pdf_name="pdfs/$2"
+pdf_download_path="$2"
+pdf_name="$pdf_download_path/$3"
 wget -O $pdf_name --user-agent="Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:132.0) Gecko/20100101 Firefox/132.0" $pdf_url
