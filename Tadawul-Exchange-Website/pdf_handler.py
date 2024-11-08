275f040e7ff6758505e790fa141e5d74490868ea
eslamdyab <eslamdyab22@gmail.com>
2024-11-08 15:55:25 +0200

    handle taking desired pdf by date and type and year, download it and parsing its tabular data to convert tot csv

--- Tadawul-Exchange-Website/pdf_handler.py
+++ Tadawul-Exchange-Website/pdf_handler.py
@@ -1,7 +1,13 @@
 import pandas as pd
 import subprocess
+import pdfplumber
+import json
+import os
+from utils import parse_pdf_page
 
 def download_pdf(year, date, type):
+    pdf_download_path = os.getcwd()+'/pdfs'
+
     if type == 'monthly':
         base_path = 'reports_urls_csvs/Monthly Trading By Nationality/'
         pdf_file_name = date + '-Monthly.pdf'
@@ -10,21 +16,36 @@ def download_pdf(year, date, type):
         base_path = 'reports_urls_csvs/Weekly Trading By Nationality/'
         pdf_file_name = date + '-Weekly.pdf'
 
-
+    
     path = base_path + year + '.csv'
     df = pd.read_csv(path)
 
     pdf_url = df[df['date'] == date]['url'].values[0]
 
-    subprocess.check_call(["./wget.sh", pdf_url, pdf_file_name])
+    if(pdf_file_name in os.listdir(pdf_download_path)):
+        print(pdf_file_name + ' is downloaded before, skipping download step......')
+    else:
+        subprocess.check_call(["./wget.sh", pdf_url, pdf_download_path, pdf_file_name])
+
+    return 'pdfs/' + pdf_file_name
 
 
+
+def convert_pdf_to_csv(pdf_file_path): 
+    with pdfplumber.open(pdf_file_path) as pdf:
+        # page 5
+        table_data = parse_pdf_page(pdf.pages[4])
+        print(json.dumps(table_data, indent=1))
+        
+        # # page 6
+        # table_data = parse_pdf_page(pdf.pages[5])
 
 
 
 
 def main():
-    download_pdf(year = '2023', date = '31-07-2023', type = 'monthly')
+    pdf_file_path = download_pdf(year = '2024', date = '30-09-2024', type = 'monthly')
+    convert_pdf_to_csv(pdf_file_path)
 
 
 main()
---
+++ Tadawul-Exchange-Website/utils.py
@@ -0,0 +1,107 @@
+skip_lines = [
+        "Monthly Report - Main Market",
+        "Value Traded (by Nationality and Investor Type)",
+        "Main Market Value Traded Breakdown - By Nationality and Investor Type",
+        "Buy Sell Net Value Traded",
+        "Nationality Investor Type",
+        "SAR Difference",
+        "SAR % of Total Buys SAR % of Total Sells",
+        "(Buy - Sell) (% Buy - % Sell)",
+        "Value Traded (by Investor Classification)",
+        "Main Market Value Traded Breakdown - By Investor Classification"
+    ]
+
+
+def parse_pdf_page(page):
+    table_data = []
+    current_section = None
+
+    
+    for line in page.extract_text().split('\n'):
+        line = line.strip()
+        
+        if line in skip_lines:
+            continue
+
+        if line == 'Individuals:':
+            current_section = 'Individuals'
+            nationality = 'Saudi'
+        
+        elif line == 'Saudi Institutions:':
+            current_section = 'Institutions'
+            nationality = 'Saudi'
+
+        elif line == 'Total Saudi Investors':
+            current_section = 'Total Saudi Investors'
+            nationality = 'Saudi'
+
+        elif line.split()[0] == 'Individuals':
+            current_section = None
+            nationality = 'GCC'
+
+        elif 'SWAP Holders' in line:
+            current_section = None
+            nationality = 'Foreign'
+
+            
+        elif 'Grand Total' in line:
+            current_section = 'Grand Total'
+            nationality = 'Grand Total'
+
+
+        if len(line.split()) > 2 and current_section != 'Grand Total':
+            splitted_table_data = line.split()
+
+            if ',' in splitted_table_data[1]:
+                investor_type = splitted_table_data[0]
+            elif ',' in splitted_table_data[2]:
+                investor_type = ' '.join(splitted_table_data[0:2])
+                del splitted_table_data[1]
+            elif ',' in splitted_table_data[3]:
+                investor_type = ' '.join(splitted_table_data[0:3])
+                del splitted_table_data[1:3]
+            elif ',' in splitted_table_data[4]:
+                investor_type = ' '.join(splitted_table_data[0:4])
+                del splitted_table_data[1:4]
+    
+
+            buy_sar = splitted_table_data[1]
+            buy_of_total_buys = splitted_table_data[2]
+            sel_sar = splitted_table_data[3]
+            sel_of_total_buys = splitted_table_data[4]
+            net_value_traded_sar = splitted_table_data[5]
+            net_value_traded_difference = splitted_table_data[6]
+
+            net_value_traded_difference = splitted_table_data[6]
+
+            if current_section == None:
+                investor_type_string = investor_type
+            else:
+                investor_type_string = current_section + '-' + investor_type
+
+            table_data.append({ 'Nationality': nationality,
+                                'Investor Type': investor_type_string, 
+                                'Buy SAR':buy_sar, 
+                                'Buy % of Total Buys':buy_of_total_buys,
+                                'Sell SAR': sel_sar,
+                                'Sel % of Total Buys':sel_of_total_buys,
+                                'Net Value Traded SAR (Buy - Sell)':net_value_traded_sar,
+                                'Net Value Traded Difference (% Buy - % Sell)':net_value_traded_difference})
+
+
+
+        elif current_section == 'Grand Total' and len(line.split()) > 2:
+            splitted_table_data = line.split()
+
+
+            table_data.append({ 'Nationality': nationality,
+                                'Investor Type': None,
+                                'Buy SAR':buy_sar, 
+                                'Buy % of Total Buys':buy_of_total_buys,
+                                'Sell SAR': sel_sar,
+                                'Sel % of Total Buys':sel_of_total_buys,
+                                'Net Value Traded SAR (Buy - Sell)':None,
+                                'Net Value Traded Difference (% Buy - % Sell)':None})
+            
+
+    return table_data
